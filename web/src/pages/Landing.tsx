77f0d3849ae2963ba07e643cb54a65753a7674a6
FelipePardim <felipesantos.2017@alunos.utfpr.edu.br>
2021-01-29 22:49:17 -0300

    Login button

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -13,15 +13,19 @@ function Landing() {
       <div className="content-wrapper">
         <img src={logoImg} alt="Happy" />
 
+        <div className="location">
+          <strong>Dois Vizinhos</strong>
+          <span>Paraná</span>
+        </div>
+
         <main>
           <h1>Leve Felicidade para o mundo</h1>
           <p>Visite orfanatos e mude o dia de muitas crianças.</p>
         </main>
 
-        <div className="location">
-          <strong>São Paulo</strong>
-          <span>São Paulo</span>
-        </div>
+        <Link to="/" className="access">
+          Acessar
+        </Link>
 
         <Link to="/app" className="enter-app">
           <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
